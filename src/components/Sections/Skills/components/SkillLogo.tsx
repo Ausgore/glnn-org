b1a977307c8d1dfe8a4197acd3cb8f1b6064605b
Ausgore <133960423+Ausgore@users.noreply.github.com>
2023-07-03 18:01:01 +1000

    Removed SkillLogo

--- src/components/Sections/Skills/components/SkillLogo.tsx
+++
@@ -1,9 +0,0 @@
-import { IconType } from 'react-icons/lib/esm/iconBase';
-interface SkillLogoProps {
-	logo: IconType;
-	color?: string;
-}
-
-export default function SkillLogo({ logo: IconComponent, color }: SkillLogoProps) {
-	return <div className="m-auto p-1"> <IconComponent size="100%" color={color} /> </div>;
-}

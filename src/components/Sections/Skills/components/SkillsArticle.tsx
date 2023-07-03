21647e2484de31f6b5b4a5c287e0fc018aaa82b4
Ausgore <133960423+Ausgore@users.noreply.github.com>
2023-07-03 17:12:49 +1000

    Setting width and height of skill icon
    It is stated that the width and height must be specified in the img for the svg to actually display. I'll have to deploy this and view on mobile to actually see if it works, but hopefully it does.

--- src/components/Sections/Skills/components/SkillsArticle.tsx
+++ src/components/Sections/Skills/components/SkillsArticle.tsx
@@ -11,7 +11,7 @@ export default function SkillsArticle(props: SkillsArticleProps) {
 	return (
 		<article className="m-4">
 			<ArticleHeader> {props.title} </ArticleHeader>
-			<ArticleGrid> {props.logos.map(logo => <img src={`https://cdn.simpleicons.org/${logo}`} className="m-2" />)} </ArticleGrid>
+			<ArticleGrid> {props.logos.map(logo => <img src={`https://cdn.simpleicons.org/${logo}`} width="60px" height="auto" className="m-auto p-1 md:p-0 md:pb-2" />)} </ArticleGrid>
 		</article>
 	);
 }
